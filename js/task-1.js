f09db80e034ef6d4a70777295edbecaeb800d194
ddooss81 <144255208+ddooss81@users.noreply.github.com>
2023-12-21 11:27:59 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,11 @@
 'use strict';
 
-function createReversedArray(args) {
-    
-    args = Array.from(arguments);
-    //console.log(args.length)
-    let result = [];
-    //let i = args.length - 1;
-    for (let i = args.length - 1; i >= 0; i--) {
-        result.push(args[i]);
-    }
-        return result;
-  
-    }
+function slugify(title) {
+    title.toLowerCase()
+    return title.toLowerCase().replaceAll(" ", "-");
+}
 
-
-console.log(createReversedArray(12, 85, 37, 4));
-console.log(createReversedArray(164, 48, 291));
-console.log(createReversedArray(412, 371, 94, 63, 176));
-console.log(createReversedArray());
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,19 @@
 'use strict';
 
+function filterArray(numbers, value) {
+    let finalArray = [];    
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            finalArray.push(numbers[i]);
+        } else {
+            finalArray = [];
+        }
+    }
+    return finalArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
